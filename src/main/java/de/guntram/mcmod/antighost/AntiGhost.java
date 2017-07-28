4938208df15634a4defbaa01e477c05d06065e9e
Guntram Blohm <gbl@guntram.de>
2017-07-28 21:03:42 +0200

    first working version

--- src/main/java/de/guntram/mcmod/antighost/AntiGhost.java
+++ src/main/java/de/guntram/mcmod/antighost/AntiGhost.java
@@ -1,5 +1,17 @@
 package de.guntram.mcmod.antighost;
 
+import java.util.ArrayList;
+import java.util.List;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.network.NetHandlerPlayClient;
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommand;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.network.play.client.CPacketPlayerDigging;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraftforge.client.ClientCommandHandler;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
@@ -13,7 +25,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 	acceptedMinecraftVersions = "[1.12]"
 )
 
-public class AntiGhost
+public class AntiGhost implements ICommand
 {
     static final String MODID="antighost";
     static final String VERSION="1.0";
@@ -21,6 +33,7 @@ public class AntiGhost
     public void init(FMLInitializationEvent event)
     {
         MinecraftForge.EVENT_BUS.register(this);
+        ClientCommandHandler.instance.registerCommand(this);
     }
 
     @EventHandler
@@ -28,5 +41,59 @@ public class AntiGhost
         ConfigurationHandler confHandler = ConfigurationHandler.getInstance();
         confHandler.load(event.getSuggestedConfigurationFile());
         MinecraftForge.EVENT_BUS.register(confHandler);
+    }
+
+    @Override
+    public String getName() {
+        return "ghost";
+    }
+
+    @Override
+    public String getUsage(ICommandSender sender) {
+        return "/ghost";
+    }
+
+    @Override
+    public List<String> getAliases() {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
+        Minecraft mc=Minecraft.getMinecraft();
+        NetHandlerPlayClient conn = mc.getConnection();
+        if (conn==null)
+            return;
+        BlockPos pos=sender.getPosition();
+        for (int dx=-4; dx<=4; dx++)
+            for (int dy=-4; dy<=4; dy++)
+                for (int dz=-4; dz<=4; dz++) {
+                    CPacketPlayerDigging packet=new CPacketPlayerDigging(
+                            CPacketPlayerDigging.Action.ABORT_DESTROY_BLOCK, 
+                            new BlockPos(pos.getX()+dx, pos.getY()+dy, pos.getZ()+dz),
+                            EnumFacing.UP       // with ABORT_DESTROY_BLOCK, this value is unused
+                    );
+                    conn.sendPacket(packet);
+                }
+    }
+
+    @Override
+    public boolean checkPermission(MinecraftServer server, ICommandSender sender) {
+        return true;
+    }
+
+    @Override
+    public List<String> getTabCompletions(MinecraftServer server, ICommandSender sender, String[] args, BlockPos targetPos) {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public boolean isUsernameIndex(String[] args, int index) {
+        return false;
+    }
+
+    @Override
+    public int compareTo(ICommand o) {
+        return getName().compareTo(o.getName());
     }
 }
