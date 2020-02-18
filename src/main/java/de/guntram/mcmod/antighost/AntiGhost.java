ae070ecb8f8060a2933ca2cf11020b562e3829a5
Guntram Blohm <gbl@guntram.de>
2020-02-18 19:41:10 +0100

    Port to Fabric 1.15

--- src/main/java/de/guntram/mcmod/antighost/AntiGhost.java
+++ src/main/java/de/guntram/mcmod/antighost/AntiGhost.java
@@ -1,118 +1,77 @@
 package de.guntram.mcmod.antighost;
 
-import java.util.ArrayList;
-import java.util.List;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.entity.EntityPlayerSP;
-import net.minecraft.client.network.NetHandlerPlayClient;
-import net.minecraft.client.resources.I18n;
-import net.minecraft.client.settings.KeyBinding;
-import net.minecraft.command.ICommand;
-import net.minecraft.command.ICommandSender;
-import net.minecraft.network.play.client.CPacketPlayerDigging;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.util.EnumFacing;
+import com.mojang.brigadier.CommandDispatcher;
+import static io.github.cottonmc.clientcommands.ArgumentBuilders.literal;
+import io.github.cottonmc.clientcommands.ClientCommandPlugin;
+import io.github.cottonmc.clientcommands.CottonClientCommandSource;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.keybinding.FabricKeyBinding;
+import net.fabricmc.fabric.api.client.keybinding.KeyBindingRegistry;
+import net.fabricmc.fabric.api.event.client.ClientTickCallback;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.server.network.packet.PlayerActionC2SPacket;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.text.TextComponentString;
-import net.minecraftforge.client.ClientCommandHandler;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.client.registry.ClientRegistry;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.InputEvent;
-import org.lwjgl.input.Keyboard;
+import net.minecraft.util.math.Direction;
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_G;
 
-@Mod(modid = AntiGhost.MODID, 
-        version = AntiGhost.VERSION,
-	clientSideOnly = true, 
-	guiFactory = "de.guntram.mcmod.antighost.GuiFactory",
-	acceptedMinecraftVersions = "[1.12]"
-)
-
-public class AntiGhost implements ICommand
+public class AntiGhost implements ClientModInitializer, ClientCommandPlugin
 {
     static final String MODID="antighost";
     static final String VERSION="1.1";
-    static KeyBinding showGui;
+    static FabricKeyBinding showGui;
     
-    @EventHandler
-    public void init(FMLInitializationEvent event)
+    @Override
+    public void onInitializeClient()
     {
-        MinecraftForge.EVENT_BUS.register(this);
-        ClientCommandHandler.instance.registerCommand(this);
-        ClientRegistry.registerKeyBinding(showGui = new KeyBinding("key.reveal", Keyboard.KEY_G, "key.categories.antighost"));
+        final String category="key.categories.antighost";
+        KeyBindingRegistry.INSTANCE.addCategory(category);
+        KeyBindingRegistry.INSTANCE.register(
+            showGui = FabricKeyBinding.Builder
+                .create(new Identifier("key.reveal"), InputUtil.Type.KEYSYM,
+                        GLFW_KEY_G, category)
+                .build());
+        ClientTickCallback.EVENT.register(e->keyPressed());
+    }
+    
+    
+    @Override
+    public void registerCommands(CommandDispatcher<CottonClientCommandSource> cd) {
+        cd.register(
+            literal("ghost").executes(c->{
+                this.execute();
+                return 1;
+            })
+        );
     }
 
-    @EventHandler
-    public void preInit(final FMLPreInitializationEvent event) {
-        ConfigurationHandler confHandler = ConfigurationHandler.getInstance();
-        confHandler.load(event.getSuggestedConfigurationFile());
-        MinecraftForge.EVENT_BUS.register(confHandler);
-    }
-
-    @SubscribeEvent
-    public void keyPressed(final InputEvent.KeyInputEvent e) {
-        EntityPlayerSP player = Minecraft.getMinecraft().player;
+    public void keyPressed() {
+        ClientPlayerEntity player = MinecraftClient.getInstance().player;
         if (showGui.isPressed()) {
-            this.execute(null, player, null);
-            player.sendMessage(new TextComponentString(I18n.format("msg.request", (Object[]) null)));
+            this.execute();
+            player.sendMessage(new TranslatableText("msg.request"));
         }
     }
     
-    @Override
-    public String getName() {
-        return "ghost";
-    }
-
-    @Override
-    public String getUsage(ICommandSender sender) {
-        return "/ghost";
-    }
-
-    @Override
-    public List<String> getAliases() {
-        return new ArrayList<>();
-    }
-
-    @Override
-    public void execute(MinecraftServer server, ICommandSender sender, String[] args) {
-        Minecraft mc=Minecraft.getMinecraft();
-        NetHandlerPlayClient conn = mc.getConnection();
+    public void execute() {
+        MinecraftClient mc=MinecraftClient.getInstance();
+        ClientPlayNetworkHandler conn = mc.getNetworkHandler();
         if (conn==null)
             return;
-        BlockPos pos=sender.getPosition();
+        BlockPos pos=mc.player.getBlockPos();
         for (int dx=-4; dx<=4; dx++)
             for (int dy=-4; dy<=4; dy++)
                 for (int dz=-4; dz<=4; dz++) {
-                    CPacketPlayerDigging packet=new CPacketPlayerDigging(
-                            CPacketPlayerDigging.Action.ABORT_DESTROY_BLOCK, 
+                    PlayerActionC2SPacket packet=new PlayerActionC2SPacket(
+                            PlayerActionC2SPacket.Action.ABORT_DESTROY_BLOCK, 
                             new BlockPos(pos.getX()+dx, pos.getY()+dy, pos.getZ()+dz),
-                            EnumFacing.UP       // with ABORT_DESTROY_BLOCK, this value is unused
+                            Direction.UP       // with ABORT_DESTROY_BLOCK, this value is unused
                     );
                     conn.sendPacket(packet);
                 }
-    }
-
-    @Override
-    public boolean checkPermission(MinecraftServer server, ICommandSender sender) {
-        return true;
-    }
-
-    @Override
-    public List<String> getTabCompletions(MinecraftServer server, ICommandSender sender, String[] args, BlockPos targetPos) {
-        return new ArrayList<>();
-    }
-
-    @Override
-    public boolean isUsernameIndex(String[] args, int index) {
-        return false;
-    }
-
-    @Override
-    public int compareTo(ICommand o) {
-        return getName().compareTo(o.getName());
     }
 }
