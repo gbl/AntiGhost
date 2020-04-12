568b3dd8a6c9e338f25d708c047bcd933da635ca
Guntram Blohm <gbl@guntram.de>
2020-04-12 12:37:57 +0200

    port to forge 1.15.2

--- src/main/java/de/guntram/mcmod/antighost/AntiGhost.java
+++ src/main/java/de/guntram/mcmod/antighost/AntiGhost.java
@@ -1,73 +1,70 @@
 package de.guntram.mcmod.antighost;
 
-import java.util.ArrayList;
-import java.util.List;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.entity.EntityPlayerSP;
-import net.minecraft.client.network.NetHandlerPlayClient;
-import net.minecraft.client.resources.I18n;
+import net.minecraft.client.entity.player.ClientPlayerEntity;
+import net.minecraft.client.network.play.ClientPlayNetHandler;
 import net.minecraft.client.settings.KeyBinding;
-import net.minecraft.command.ICommand;
-import net.minecraft.command.ICommandSender;
-import net.minecraft.network.play.client.CPacketPlayerDigging;
+import net.minecraft.command.ICommandSource;
+import net.minecraft.network.play.client.CPlayerDiggingPacket;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.util.EnumFacing;
+import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.text.TextComponentString;
-import net.minecraftforge.client.ClientCommandHandler;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraftforge.client.event.ClientChatEvent;
+import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.InputEvent;
-import org.lwjgl.input.Keyboard;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-@Mod(modid = AntiGhost.MODID, 
-        version = AntiGhost.VERSION,
-	clientSideOnly = true, 
-	guiFactory = "de.guntram.mcmod.antighost.GuiFactory",
-	acceptedMinecraftVersions = "[1.12]"
-)
 
-public class AntiGhost implements ICommand
+
+@Mod("antighost") 
+
+public class AntiGhost 
 {
     static final String MODID="antighost";
     static final String VERSION="1.1.1";
     static KeyBinding showGui;
     
-    @EventHandler
-    public void init(FMLInitializationEvent event)
+    public AntiGhost() {
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        bus.addListener(this::init);        
+    }
+    
+    public void init(FMLCommonSetupEvent event)
     {
         MinecraftForge.EVENT_BUS.register(this);
-        ClientCommandHandler.instance.registerCommand(this);
-        ClientRegistry.registerKeyBinding(showGui = new KeyBinding("key.reveal", Keyboard.KEY_G, "key.categories.antighost"));
-    }
-
-    @EventHandler
-    public void preInit(final FMLPreInitializationEvent event) {
-        ConfigurationHandler confHandler = ConfigurationHandler.getInstance();
-        confHandler.load(event.getSuggestedConfigurationFile());
-        MinecraftForge.EVENT_BUS.register(confHandler);
+        // ClientCommandHandler.instance.registerCommand(this); How the F in forge?
+        ClientRegistry.registerKeyBinding(showGui = 
+                new KeyBinding("key.reveal", 'G', "key.categories.antighost"));
     }
 
     @SubscribeEvent
     public void keyPressed(final InputEvent.KeyInputEvent e) {
-        EntityPlayerSP player = Minecraft.getMinecraft().player;
+        ClientPlayerEntity player = Minecraft.getInstance().player;
         if (showGui.isPressed()) {
             this.execute(null, player, null);
-            player.sendMessage(new TextComponentString(I18n.format("msg.request", (Object[]) null)));
+            player.sendMessage(new TranslationTextComponent("msg.request"));
         }
     }
     
-    @Override
+    @SubscribeEvent
+    public void chatEvent(final ClientChatEvent e) {
+        if (e.getOriginalMessage().equals("/ghost")) {
+            this.execute(null, Minecraft.getInstance().player, null);
+            e.setCanceled(true);
+        }
+    }
+
+/*
     public String getName() {
         return "ghost";
     }
 
-    @Override
     public String getUsage(ICommandSender sender) {
         return "/ghost";
     }
@@ -78,24 +75,30 @@ public class AntiGhost implements ICommand
     }
 
     @Override
-    public void execute(MinecraftServer server, ICommandSender sender, String[] args) {
-        Minecraft mc=Minecraft.getMinecraft();
-        NetHandlerPlayClient conn = mc.getConnection();
+*/
+    
+    public void execute(MinecraftServer server, ICommandSource sender, String[] args) {
+        Minecraft mc=Minecraft.getInstance();
+        ClientPlayNetHandler conn = mc.getConnection();
         if (conn==null)
             return;
-        BlockPos pos=sender.getPosition();
+        if (!(sender instanceof ClientPlayerEntity)) {
+            return;
+        }
+        ClientPlayerEntity player = (ClientPlayerEntity) sender;
+        BlockPos pos=player.getPosition();
         for (int dx=-4; dx<=4; dx++)
             for (int dy=-4; dy<=4; dy++)
                 for (int dz=-4; dz<=4; dz++) {
-                    CPacketPlayerDigging packet=new CPacketPlayerDigging(
-                            CPacketPlayerDigging.Action.ABORT_DESTROY_BLOCK, 
+                    CPlayerDiggingPacket packet=new CPlayerDiggingPacket(
+                            CPlayerDiggingPacket.Action.ABORT_DESTROY_BLOCK, 
                             new BlockPos(pos.getX()+dx, pos.getY()+dy, pos.getZ()+dz),
-                            EnumFacing.UP       // with ABORT_DESTROY_BLOCK, this value is unused
+                            Direction.UP       // with ABORT_DESTROY_BLOCK, this value is unused
                     );
                     conn.sendPacket(packet);
                 }
     }
-
+/*
     @Override
     public boolean checkPermission(MinecraftServer server, ICommandSender sender) {
         return true;
@@ -115,4 +118,5 @@ public class AntiGhost implements ICommand
     public int compareTo(ICommand o) {
         return getName().compareTo(o.getName());
     }
+*/
 }
