a91125703675e8e4c9264a970c7ac3fe928283c6
Guntram Blohm <gbl@guntram.de>
2020-08-02 15:19:48 +0200

    update to 1.16.2

--- src/main/java/de/guntram/mcmod/antighost/AntiGhost.java
+++ src/main/java/de/guntram/mcmod/antighost/AntiGhost.java
@@ -5,16 +5,14 @@ import static io.github.cottonmc.clientcommands.ArgumentBuilders.literal;
 import io.github.cottonmc.clientcommands.ClientCommandPlugin;
 import io.github.cottonmc.clientcommands.CottonClientCommandSource;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.keybinding.FabricKeyBinding;
-import net.fabricmc.fabric.api.client.keybinding.KeyBindingRegistry;
-import net.fabricmc.fabric.api.event.client.ClientTickCallback;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.client.network.ClientPlayerEntity;
-import net.minecraft.client.util.InputUtil;
+import net.minecraft.client.options.KeyBinding;
 import net.minecraft.network.packet.c2s.play.PlayerActionC2SPacket;
 import net.minecraft.text.TranslatableText;
-import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import static org.lwjgl.glfw.GLFW.GLFW_KEY_G;
@@ -23,18 +21,15 @@ public class AntiGhost implements ClientModInitializer, ClientCommandPlugin
 {
     static final String MODID="antighost";
     static final String VERSION="1.1";
-    static FabricKeyBinding requestBlocks;
+    static KeyBinding requestBlocks;
     
     @Override
     public void onInitializeClient()
     {
         final String category="key.categories.antighost";
-        KeyBindingRegistry.INSTANCE.addCategory(category);
-        KeyBindingRegistry.INSTANCE.register(requestBlocks = FabricKeyBinding.Builder
-                .create(new Identifier("antighost:reveal"), InputUtil.Type.KEYSYM,
-                        GLFW_KEY_G, category)
-                .build());
-        ClientTickCallback.EVENT.register(e->keyPressed());
+        requestBlocks = new KeyBinding("antighost:reveal", GLFW_KEY_G, category);
+        KeyBindingHelper.registerKeyBinding(requestBlocks);
+        ClientTickEvents.END_CLIENT_TICK.register(e->keyPressed());
     }
     
     
