6746f22eaf6abc441218a19d3f56f179605328ef
Guntram Blohm <gbl@guntram.de>
2021-07-28 16:55:27 +0200

    first 1.17 port

--- src/main/java/de/guntram/mcmod/antighost/AntiGhost.java
+++ src/main/java/de/guntram/mcmod/antighost/AntiGhost.java
@@ -1,29 +1,28 @@
 package de.guntram.mcmod.antighost;
 
+import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.entity.player.ClientPlayerEntity;
-import net.minecraft.client.network.play.ClientPlayNetHandler;
-import net.minecraft.client.settings.KeyBinding;
-import net.minecraft.command.ICommandSource;
-import net.minecraft.network.play.client.CPlayerDiggingPacket;
+import net.minecraft.client.multiplayer.ClientPacketListener;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.network.protocol.game.ServerboundPlayerActionPacket;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.util.Direction;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.client.event.ClientChatEvent;
 import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fmlclient.registry.ClientRegistry;
 
 public class AntiGhost 
 {
     static final String MODID="antighost";
     static final String MODNAME="AntiGhost";
-    static KeyBinding showGui;
+    static KeyMapping showGui;
     
     public AntiGhost() {
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
@@ -35,15 +34,15 @@ public class AntiGhost
         MinecraftForge.EVENT_BUS.register(this);
         // ClientCommandHandler.instance.registerCommand(this); How the F in forge?
         ClientRegistry.registerKeyBinding(showGui = 
-                new KeyBinding("key.reveal", 'G', "key.categories.antighost"));
+                new KeyMapping("key.reveal", 'G', "key.categories.antighost"));
     }
 
     @SubscribeEvent
     public void keyPressed(final InputEvent.KeyInputEvent e) {
-        ClientPlayerEntity player = Minecraft.getInstance().player;
-        if (showGui.isPressed()) {
+        LocalPlayer player = Minecraft.getInstance().player;
+        if (showGui.consumeClick()) {
             this.execute(null, player, null);
-            player.sendStatusMessage(new TranslationTextComponent("msg.request"), false);
+            player.displayClientMessage(new TranslatableComponent("msg.request"), true);
         }
     }
     
@@ -55,25 +54,21 @@ public class AntiGhost
         }
     }
 
-    public void execute(MinecraftServer server, ICommandSource sender, String[] args) {
+    public void execute(MinecraftServer server, LocalPlayer player, String[] args) {
         Minecraft mc=Minecraft.getInstance();
-        ClientPlayNetHandler conn = mc.getConnection();
+        ClientPacketListener conn = mc.getConnection();
         if (conn==null)
             return;
-        if (!(sender instanceof ClientPlayerEntity)) {
-            return;
-        }
-        ClientPlayerEntity player = (ClientPlayerEntity) sender;
-        BlockPos pos=player.getPosition();
+        BlockPos pos=player.blockPosition();
         for (int dx=-4; dx<=4; dx++)
             for (int dy=-4; dy<=4; dy++)
                 for (int dz=-4; dz<=4; dz++) {
-                    CPlayerDiggingPacket packet=new CPlayerDiggingPacket(
-                            CPlayerDiggingPacket.Action.ABORT_DESTROY_BLOCK, 
+                    ServerboundPlayerActionPacket packet=new ServerboundPlayerActionPacket(
+                            ServerboundPlayerActionPacket.Action.ABORT_DESTROY_BLOCK, 
                             new BlockPos(pos.getX()+dx, pos.getY()+dy, pos.getZ()+dz),
                             Direction.UP       // with ABORT_DESTROY_BLOCK, this value is unused
                     );
-                    conn.sendPacket(packet);
+                    conn.send(packet);
                 }
     }
 }
--- src/main/java/de/guntram/mcmod/antighost/AntiGhostMain.java
+++ src/main/java/de/guntram/mcmod/antighost/AntiGhostMain.java
@@ -1,11 +1,7 @@
 package de.guntram.mcmod.antighost;
 
-import net.minecraftforge.fml.ExtensionPoint;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.loading.FMLEnvironment;
-import net.minecraftforge.fml.network.FMLNetworkConstants;
-import org.apache.commons.lang3.tuple.Pair;
 
 @Mod("antighost") 
 // This is just a wrapper class to make sure the Grid class itself, which
@@ -19,7 +15,7 @@ public class AntiGhostMain {
     public AntiGhost antiGhost;
     
     public AntiGhostMain() {
-        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+        // ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
         if (FMLEnvironment.dist.isClient()) {
             antiGhost = new AntiGhost();
         } else {
